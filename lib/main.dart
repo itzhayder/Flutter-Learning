9e5d7d8eb90393b0ddc4a22942b129951ba6a297
itzhayder <mdhayder.ali@northsouth.edu>
2020-03-27 01:35:21 +0600

    All basic done up to #14 video

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,41 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("This is Flutter App"),
+        title: Text("My App"),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: Image.asset('assets/photo-2.jpg')
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(child: Image.asset('assets/photo-1.jpg')),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.redAccent,
+              child: Text("1"),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.amber,
+              child: Text("2"),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.blue,
+              child: Text("3"),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Center(
-          child: Text("Click me"),
+          child: Text("Click"),
         ),
         backgroundColor: Colors.red[600],
       ),
